f240a0b2e33e6b3e24ff4e306e08459f95c18e30
lindsayram <lindsayrpro@gmail.com>
2026-07-26 22:53:36 +0200

    feat: add responsive layout and interactive pricing features
    Update CSS with slider thumb styling, hover effects, and mobile media queries. Modify HTML to include pageview display, price symbol, and checkbox ID. Enhance JavaScript with dynamic pricing values and toggle functionality for monthly/yearly billing.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
 const input = document.querySelector('#range-bar')
-const values= ['$8.00', '$12.00', '$16.00', '$24.00', '$36.00']
-const display = document.querySelector('#display')
 
-input.max = values.length - 1;
+const valuesPrice= [8.00, 12.00, 16.00, 24.00, 36.00]
+// const valuesPriceDiscount= ['6.00', 9.00', 12.00', 18.00', 27.00']
+const valuesProduct= ['10K', '50K', '100K', '500K', '1M']
+
+const display = document.querySelector('#display')
+const display2 = document.querySelector('#display2')
+
+const toggle = document.querySelector('#checkbox')
+
+input.max = valuesPrice.length - 1;
 input.value= 2
 
 function updateValue (){
     const index = parseInt(input.value, 10);
-    display.textContent = values[index] 
+    display.textContent = valuesPrice[index].toFixed(2) 
 }
 
-input.addEventListener('input', updateValue)
+function updateValueProduct () {
+    const index = parseInt(input.value, 10)
+    display2.textContent = valuesProduct[index]
+}
 
-updateValue()
+function discount () {
+    const index = parseInt(input.value, 10)
+    display.textContent = ((valuesPrice[index])* 0.75).toFixed(2) 
+}
+
+function updateDisplay (){
+    if(toggle.checked == true) {
+        discount();
+    } else {
+        updateValue();
+    }
+    updateValueProduct()
+}
+
+input.addEventListener('input', updateDisplay)
+
+toggle.addEventListener('change', updateDisplay)
+
+updateDisplay()
+// updateValueProduct()
